f16bcab8d5a2cc0a7806687e2f322848e3a1d2b5
Ariana Barzinpour <ariana.barzinpour@ga.gov.au>
2023-09-05 06:18:53 +0000

    add Range to public objects

--- eo3/__init__.py
+++ eo3/__init__.py
@@ -1,4 +1,5 @@
 from ._version import get_versions
+from .fields import Range
 from .model import DatasetMetadata
 
 REPO_URL = "https://github.com/opendatacube/eo3.git"
@@ -8,6 +9,7 @@ del get_versions
 
 __all__ = (
     "DatasetMetadata",
+    "Range",
     "REPO_URL",
     "__version__",
 )
